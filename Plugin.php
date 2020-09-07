d92369644cd107c62df4c0c99ba580194a8d9a13
Sebastiaan Kloos <sebastiaan@kloos.dev>
2020-09-07 11:45:13 +0200

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Kloos\Auth;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Auth Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Auth',
+            'description' => 'No description provided yet...',
+            'author'      => 'Kloos',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Kloos\Auth\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'kloos.auth.some_permission' => [
+                'tab' => 'Auth',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'auth' => [
+                'label'       => 'Auth',
+                'url'         => Backend::url('kloos/auth/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['kloos.auth.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
